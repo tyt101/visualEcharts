194bbc1ecbf64f592907cf45c7ce90f7d26165da
tyt101 <1104427708@qq.com>
2022-05-08 14:50:18 +0800

    饼状图2

--- js/index.js
+++ js/index.js
@@ -413,3 +413,47 @@
         myChart.resize();
     });
 })();
+(function() {
+    let myChart = echarts.init(document.querySelector(".pie1 .chart"));
+    option = {
+        legend: {
+            bottom: "-3%",
+            itemWidth: 7,
+            itemHeight: 7,
+            textStyle: {
+                color: "rgba(255,255,255,.4)"
+            }
+        },
+        series: [{
+            name: '面积模式',
+            type: 'pie',
+            radius: ["10%", "70%"],
+            center: ['50%', '50%'],
+            roseType: 'radius',
+            itemStyle: {
+                borderRadius: 8
+            },
+            data: [
+                { value: 20, name: '云南' },
+                { value: 26, name: '北京' },
+                { value: 24, name: '山东' },
+                { value: 25, name: '河北' },
+                { value: 20, name: '江苏' },
+                { value: 25, name: '浙江' },
+                { value: 30, name: '四川' },
+                { value: 42, name: '湖北' }
+            ],
+            label: {
+                fontSize: 10
+            },
+            labelLine: {
+                length: 2,
+            },
+            color: ['#006cff', '#60cda0', '#ed8884', '#ff9f7f', '#0096ff', '#9fe6b8', '#32c5e9', '#1d9dff'],
+        }]
+    };
+    myChart.setOption(option);
+    window.addEventListener("resize", function() {
+        myChart.resize();
+    });
+})();
